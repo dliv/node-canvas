e30f14be897be982b682f598ab51a2b56cc3f43c
David Livingston <david.livingston@nvc-inc.net>
2021-10-05 11:32:58 -0500

    change binary host to localhost

---
+++ server.py
@@ -0,0 +1,11 @@
+# https://stackoverflow.com/a/19706670
+import http.server, ssl
+
+server_address = ('localhost', 8000)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='localhost.pem',
+                               ssl_version=ssl.PROTOCOL_TLS)
+httpd.serve_forever()
+
